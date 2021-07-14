b5cb9dc69838d76db61f4a7c2fb22a80c3637f13
Sitt Aung <sittaung2021@gmail.com>
2021-07-14 22:52:56 +0630

    Recent Orders

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_ui_flutter/data/data.dart';
+import 'package:food_delivery_ui_flutter/widgets/recent_orders.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -62,6 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
+          RecentOrders()
         ],
       ),
     );
